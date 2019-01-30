189f186ae1b4a1df3065472660b432d18859dbec
github username <github email address>
2019-01-30 17:16:08 -0500

    advanced methods and classes done

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,67 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = Song.new
+    song.save
+    song
+    # binding.pry
+  end
+
+  def self.new_by_name(song_name)
+    song = Song.new
+    song.name = song_name
+    song
+  end
+
+  def self.create_by_name(song_name)
+    song = Song.create
+    song.name = song_name
+    song
+    # binding.pry
+  end
+
+  def self.find_by_name(song_name)
+    @@all.find do |song_list|
+      song_list.name == song_name
+    end
+    # binding.pry
+  end
+
+  def self.find_or_create_by_name(song_name)
+    self.find_by_name(song_name) || self.create_by_name(song_name)
+  end
+
+  def self.alphabetical
+    songs = []
+    songs = @@all
+    songs.sort_by do |song|
+      song.name
+    end
+  end
+
+  def self.new_from_filename(song_filename)
+    song = Song.create
+    filename = song_filename.split("-")
+    artist = filename[0].strip
+    song_name = filename[1].split(".")[0].strip
+    song.name = song_name
+    song.artist_name = artist
+    song
+  end
+
+  def self.create_from_filename(song_filename)
+    song = Song.create
+    filename = song_filename.split("-")
+    artist = filename[0].strip
+    song_name = filename[1].split(".")[0].strip
+    song.name = song_name
+    song.artist_name = artist
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
 end
