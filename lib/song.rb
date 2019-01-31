46da6f7d27cb2b5b3cde1cf8e5f0e38f307291fd
github username <github email address>
2019-01-30 23:08:35 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,7 @@
+require 'pry'
+
 class Song
+
   attr_accessor :name, :artist_name
   @@all = []
 
@@ -6,8 +9,65 @@ class Song
     @@all
   end
 
-  def save
-    self.class.all << self
+  def self.create
+    song = self.new
+    self.all << song
+    song
+  end
+
+  # def save
+  #   self.class.all << self
+  # end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    self.all << song
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.create
+    song.name = name
+    song
+  end
+
+  def self.find_by_name(name)
+    self.all.find {|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    if !self.find_by_name(name)
+      self.create_by_name(name)
+    else
+      self.find_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+    self.all.sort_by { |song| song.name }
+  end
+
+  def self.new_from_filename(filename)
+    splited = filename.split(/[\.\-]/)
+    striped = splited.each { |element| element.strip! }
+
+    song = self.new_by_name(striped[1])
+    song.artist_name = striped[0]
+    song
+  end
+
+  def self.create_from_filename(filename)
+    splited = filename.split(/[\.\-]/)
+    striped = splited.each { |element| element.strip! }
+
+    song = self.create_by_name(striped[1])
+    song.artist_name = striped[0]
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
   end
 
 end
